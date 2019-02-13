958866ec11ca9941e9115cd4bd441127de27f8b4
EVaughan00 <evanvaughan9@gmail.com>
2019-02-12 17:26:16 -0700

    Updated Visuals

--- server/config/keys.js
+++ server/config/keys.js
@@ -6,8 +6,8 @@ module.exports = {
     clientSecret:'KBB81kEh9Xcz_kmsC6M5nD05'
   },
   mongodb: {
-    // 'mongodb://evDB:Wisdom2b@ds141450.mlab.com:41450/evdb'
-    dbURI:  'mongodb://localhost:27017/evdb'
+    dbURI: 'mongodb://evDB:Wisdom2b@ds141450.mlab.com:41450/evdb'
+    // 'mongodb://localhost:27017/evdb'
   },
   session: {
     cookieKey: 'SomethingSomethingThisisAwesome'
